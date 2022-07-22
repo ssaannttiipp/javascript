7e9f699f054f345f11bf7b5689c3e910d2a69354
Santiago Perez <santip422@gmail.com>
2022-07-22 01:14:27 -0300

    desafio dom y evento

--- DesafioJava/index.js
+++ DesafioJava/index.js
@@ -1,11 +1,49 @@
+let contenedor = document.getElementById("container");
+let productos = [
+    { id: 1, nombre: "Parrilla Modular", precio: 7000 },
+    { id: 2, nombre: "Disco", precio: 8300 },
+    { id: 3, nombre: "Fogonero", precio: 15600 },
+    { id: 4, nombre: "Tabla", precio: 1500 },
+    { id: 5, nombre: "Tabla Gravada", precio: 1800 },
+    { id: 6, nombre: "Set Asador", precio: 1200 },
+];
+
+for (const producto of productos) {
+    let li = document.createElement("li");
+    li.innerHTML = `<h5> <b>${producto.id}  <h5>${producto.nombre} <b>$${producto.precio}</b> </h5> `;
+    contenedor.append(li);
+}
+
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e){
+  e.preventDefault();
+
+  if(e.target.children[1].value.includes("-") && e.target.children[1].value.includes(".")){
+    let mensaje = document.createElement("div");
+    mensaje.innerHTML = "Bienvenido";
+    mensaje.className = "verde";
+    document.body.append(mensaje);
+  }else{
+    let mensaje = document.createElement("div");
+    mensaje.innerHTML = "Por favor seleccione un correo existente";
+    mensaje.className = "rojo";
+    document.body.append(mensaje);
+    e.target.children[1].value = "";
+  }
+}
+
+
 let nombreProducto = 'Producto';
 var precioCompras = []; //creamos el array para que simule el canasto con los precios...
 var precioTotal = 0;
 var bandera = true;
 while (bandera) {
     //Tomamos la compra y la metemos al canasto(array)
+
     nombreProducto = prompt('Elegi el producto para tu asador que desea comprar:'); //asignamos el nombre del combo
-    
+
     var precioSiguienteCompra = parseInt(prompt("Indique el precio del elemento a comprar: "));
     if (isNaN(precioSiguienteCompra)) {
         break;
@@ -21,6 +59,7 @@ while (bandera) {
         calcularDescuento();
         bandera = false;
     }
+    
 }
 
 //funcion para calcular el precio total e informmar
@@ -42,9 +81,9 @@ function calcularDescuento() {
             var descuento = parseInt(prompt("Indique el descuento a aplicar  (20-35): "));
             if (isNaN(descuento) && 20 < descuento < 35) {
                 alert("Datos invalidos");
-            } else {              
-                precioTotal = precioTotal - precioTotal * 0.01 * descuento ;
-                alert( 'El precio final con descuento es: $' +  precioTotal);
+            } else {
+                precioTotal = precioTotal - precioTotal * 0.01 * descuento;
+                alert('El precio final con descuento es: $' + precioTotal);
                 banderaDescuento = false;
             }
         }
@@ -52,4 +91,6 @@ function calcularDescuento() {
     } else {
         return;
     }
+
 }
+comprar ()
